30ee617ee774db6ab7024119a443d5e360428327
Yuta Koreeda <koreyou@mac.com>
2017-06-17 13:54:10 +0900

    added auto importing of loader modules to __init__. This fixes ImportError when modules are loaded in some order.

--- word_embedding_loader/loader/__init__.py
+++ word_embedding_loader/loader/__init__.py
@@ -3,3 +3,8 @@ loader module provides actual implementation of the file loaders.
 """
 
 __all__ = ["glove", "vocab", "word2vec_bin", "word2vec_text"]
+
+from word_embedding_loader.loader import glove
+from word_embedding_loader.loader import vocab
+from word_embedding_loader.loader import word2vec_bin
+from word_embedding_loader.loader import word2vec_text
