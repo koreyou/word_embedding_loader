01eb82ab7aa9405d49074592353bf7ec50ee6225
Yuta Koreeda <koreyou@mac.com>
2017-06-19 22:39:51 +0900

    Added help notes to cli

--- word_embedding_loader/cli.py
+++ word_embedding_loader/cli.py
@@ -28,14 +28,20 @@ def cli():
 @cli.command()
 @click.argument('inputfile', type=click.Path(exists=True))
 @click.argument('outputfile', type=click.Path())
-@click.option('-t', '--to-format', type=click.Choice(_output_choices.keys()))
-@click.option('-f', '--from-format', type=click.Choice(_input_choices.keys()), default='auto')
+@click.option('-t', '--to-format', type=click.Choice(_output_choices.keys()),
+              help='Target format')
+@click.option('-f', '--from-format', type=click.Choice(_input_choices.keys()),
+              default='auto', help='Source format. It will guess format from content if not given.')
 def convert(outputfile, inputfile, to_format, from_format):
+    """
+    Convert pretrained word embedding file in one format to another.
+    """
     emb = word_embedding.WordEmbedding.load(
         inputfile, format=_input_choices[from_format][1],
         binary=_input_choices[from_format][2])
     emb.save(outputfile, format=_output_choices[to_format][1],
              binary=_output_choices[to_format][2])
+
 
 def _echo_format_result(name):
     click.echo("{}: {}".format(name, _input_choices[name][0]))
@@ -44,6 +50,9 @@ def _echo_format_result(name):
 @cli.command()
 @click.argument('inputfile', type=click.File())
 def check_format(inputfile):
+    """
+    Check format of inputfile.
+    """
     t = word_embedding.classify_format(inputfile)
     if t == word_embedding._glove:
         _echo_format_result('glove')
@@ -57,6 +66,9 @@ def check_format(inputfile):
 
 @cli.command()
 def list():
+    """
+    List available format.
+    """
     choice_len = max(map(len, _input_choices.keys()))
     tmpl = "  {:<%d}: {}\n" % choice_len
     text = ''.join(map(
