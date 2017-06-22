63973d3912068c0edad05becd86dc4b4c5cff4d5
Yuta Koreeda <koreyou@mac.com>
2017-06-22 22:55:27 +0900

    Moved cythonize command to build_ext; REASON: cythonized files are required in build_ext not build_py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import os
 
 from setuptools import setup
 from setuptools.extension import Extension
-from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext as _build_ext
+
 
 
 cython_modules = [
@@ -10,23 +11,30 @@ cython_modules = [
     ["word_embedding_loader", "saver", "word2vec_bin"]
 ]
 
-class BuildPyCommand(build_py):
+ext_modules = [
+    Extension(
+        '.'.join(mod), ['/'.join(mod) + '.cpp'],
+        language="c++"
+    ) for mod in cython_modules
+]
+
+class BuildPyCommand(_build_ext):
     """ Custom build routine """
+#    def run(self):
+
     def run(self):
         import numpy
         from Cython.Build import cythonize
-        ext_modules = [
-            Extension(
-                '.'.join(mod), ['/'.join(mod) + '.pyx'],
-                include_dirs=[numpy.get_include()], language="c++"
-            ) for mod in cython_modules
-        ]
-        # Add signiture for Sphinx
-        for e in ext_modules:
-            e.cython_directives = {"embedsignature": True}
-
-        cythonize(ext_modules)
-        build_py.run(self)
+        _ext_modules = [Extension(
+            '.'.join(mod), ['/'.join(mod) + '.cpp'],
+            language="c++", include_dirs=[numpy.get_include()]
+        ) for mod in cython_modules]
+        for i in xrange(len(self.extensions)):
+            self.extensions[i].include_dirs.append(numpy.get_include())
+            # Add signiture for Sphinx
+            self.extensions[i].cython_directives = {"embedsignature": True}
+        cythonize(_ext_modules)
+        _build_ext.run(self)
 
 try:
     with open('README.rst') as f:
@@ -53,8 +61,9 @@ setup(
               'word_embedding_loader.loader',
               'word_embedding_loader.saver'
               ],
+    ext_modules=ext_modules,
     license='MIT',
-    cmdclass = {'build_py': BuildPyCommand},
+    cmdclass = {'build_ext': BuildPyCommand},
     install_requires=[
         'Click',
         'numpy'
