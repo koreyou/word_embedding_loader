ec7d1e504206512529a9133e6773a0b1da4d8ee1
Yuta Koreeda <koreyou@mac.com>
2017-07-25 23:28:22 +0900

    Added python 3 to setup.py classifiers.
    This denotes (on pypi) that this package is compatible with python 3.
    This should NOT add any requirements to pip; python 2.6 or 3.2
    should still be able to install the package (which may or may not
    work).

--- setup.py
+++ setup.py
@@ -124,6 +124,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Cython",
         "Topic :: Documentation :: Sphinx",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
