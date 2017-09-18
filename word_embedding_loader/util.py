a2789f4685152bcff3bf9b824dc483241df78068
Yuta Koreeda <koreyou@mac.com>
2017-09-18 12:59:51 +0900

    [Feature] Implemented inserting/moving/removal of vocabs

--- word_embedding_loader/util.py
+++ word_embedding_loader/util.py
@@ -51,3 +51,104 @@ def create_unk_least_common(we, n):
 def create_random_vector(size, std, dtype):
     assert std > 0.
     return numpy.random.normal(0., std, (size, )).astype(dtype)
+
+
+def move_vocab(vocab, vectors, key, index):
+    """
+    Move the particular word to specified index.
+
+    .. warning:: This function has side effect on ``vocab``.
+
+    Args:
+        vocab (dict): Mapping from word (bytes) to index (int)
+        vectors (numpy.ndarray): Word embedding vectors
+        key (bytes): New word to insert to move
+        index (int): Index to which ``vec`` is inserted
+
+    Returns:
+        dict: Modified vocab file
+        numpy.ndarray: Modified vectors
+        int or None: Original index of the key in vocab
+
+    """
+    if key not in vocab:
+        raise KeyError('key %s does no exist in vocab.' % key)
+    vec = vectors[vocab[key]]
+    vectors = numpy.delete(vectors, vocab[key], axis=0)
+    vectors = numpy.insert(vectors, index, vec, axis=0)
+
+    old_ind = vocab[key]
+    if old_ind == index:
+        return vocab, vectors, index
+    elif old_ind > index:
+        for k in vocab.keys():
+            if vocab[k] >= index and vocab[k] < old_ind:
+                vocab[k] += 1
+    else:
+        for k in vocab.keys():
+            if vocab[k] > old_ind and vocab[k] <= index:
+                vocab[k] -= 1
+    vocab[key] = index
+    return vocab, vectors, old_ind
+
+
+def insert_vocab(vocab, vectors, key, index, vec):
+    """
+    Insert new word to word embedding.
+
+    .. warning:: This function has side effect on ``vocab``.
+
+    Args:
+        vocab (dict): Mapping from word (bytes) to index (int)
+        vectors (numpy.ndarray): Word embedding vectors
+        key (bytes): New word to insert to move
+        index (int): Index to which ``vec`` is inserted
+        vec (numpy.ndarray): A vector to insert
+
+    Returns:
+        dict: Modified vocab file
+        numpy.ndarray: Modified vectors
+
+    """
+    assert vec.ndim == 1 or (vec.ndim == 2 and vec.shape[0] == 1)
+    assert vec.shape[-1] == vectors.shape[1]
+    if key in vocab:
+        raise KeyError('key %s already exists in vocab.' % key)
+
+    for k in vocab.keys():
+        if vocab[k] >= index:
+            vocab[k] += 1
+
+    vocab[key] = index
+    vectors = numpy.insert(vectors, index, vec, axis=0)
+    return vocab, vectors
+
+
+def remove_vocab(vocab, vectors, key):
+    """
+    Move the particular word to specified index.
+
+    .. warning:: This function has side effect on ``vocab``.
+
+    Args:
+        vocab (dict): Mapping from word (bytes) to index (int)
+        vectors (numpy.ndarray): Word embedding vectors
+        key (bytes): New word to insert to move
+        index (int): Index to which ``vec`` is inserted
+
+    Returns:
+        dict: Modified vocab file
+        numpy.ndarray: Modified vectors
+        int or None: Original index of the key in vocab
+
+    """
+    if key not in vocab:
+        raise KeyError('key %s does no exist in vocab.' % key)
+    vectors = numpy.delete(vectors, vocab[key], axis=0)
+
+    old_ind = vocab[key]
+    for k in vocab.keys():
+        if vocab[k] > old_ind:
+            vocab[k] -= 1
+    del vocab[key]
+    return vocab, vectors, old_ind
