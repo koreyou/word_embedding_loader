3959aec8382539d9e87df1774c68f4d8ef5bd9ce
Yuta Koreeda <koreyou@mac.com>
2017-11-05 14:52:11 +0900

    [Feature] Added handy loader for NN.

--- word_embedding_loader/word_embedding.py
+++ word_embedding_loader/word_embedding.py
@@ -270,3 +270,201 @@ class WordEmbedding(object):
                     del self.freqs[k]
         return self
 
+    def apply_freqs(self, freqs=None):
+        if freqs is None:
+            if self.freqs is None:
+                raise ValueError("You must supply freqs when self.freqs is None")
+            return self.apply_freqs(self.freqs)
+        if len(freqs) != len(self.vocab):
+            raise ValueError("len(freqs) != len(self.vocab)")
+        self.freqs = freqs
+        new_vocab = {}
+        # sort then reverse for stability
+        freqs_list = sorted(six.iteritems(freqs), key=lambda k_v: k_v[1])
+        for i, (k, _) in enumerate(freqs_list[::-1]):
+            new_vocab[k] = i
+
+
+def load_word_embedding(
+        path, vocab=None, dtype=np.float32, max_vocab=None,
+        format=None, binary=False, unk=b'<unk>', unk_index=0, eos=b'</s>',
+        eos_index=1, unk_creation_method='least_common',
+        unk_creation_n_least=10, random_init_std='auto', random_init_seed=0):
+    """
+    Load pretrained word embedding from a file. This is short hand method for
+    using :func:`~word_embedding_loader.word_embedding.WordEmbedding.load` and
+    getting :attr:`~word_embedding_loader.word_embedding.WordEmbedding.vectors`
+    and :attr:`~word_embedding_loader.word_embedding.WordEmbedding.vocab`.
+    This is especially useful if you have no plans to save the word embedding.
+
+    By specifying ``unk`` or ``eos``, you are guaranteed to have a embedding
+    for out-of-vocabulary words and for the end-of-sentence token, respectively.
+    The function first tries to detect if those tokens already exists by
+    looking for words that are they tend to be associated with (e.g. "</s>").
+    On success, it replaces those found vocabularies with the words you have
+    provided to the function. On failure, it initializes and append new
+    embeddings to the word embedding.
+
+    Args:
+        path (str): Path of file to load.
+        vocab (str or set or None): Path to vocab files or set of vocab to use.
+            Refer
+            :func:`~word_embedding_loader.word_embedding.WordEmbedding.load`
+            for details.
+        dtype (numpy.dtype): Element data type to use for the array.
+        max_vocab (int): Number of vocabulary to read.
+        format (str or None): Format of the file. Refer
+            :func:`~word_embedding_loader.word_embedding.WordEmbedding.load` for
+            details.
+        binary (bool): Load file as binary file.  Refer
+            :func:`~word_embedding_loader.word_embedding.WordEmbedding.load` for
+            details.
+        unk (bytes or None): The vocabulary for out-of-vocabulary words.
+            If ``None`` it will not do any post-precessings to gurentee that
+            it exists.
+        unk_index (int): Index to which unk is inserted.
+        eos (bytes or None): The vocabulary for the end-of-sentence token.
+            If ``None`` it will not do any post-precessings to gurentee that
+            it exists.
+        eos_index (int): Index to which eos is inserted.
+        unk_creation_method (str): The method to use if it needs to create
+            an embedding for out-of-vocabulary words. ``'least_common'`` will
+            take n least common word embeddings and take their average (it will
+            use frequencies from vocab file if provided or assume that words
+            are ordered in the order of frequencies). ``random`` will create
+            word embedding normal distribution drawn with the
+            ``random_init_std`` argument. This argument is ignored if ``unk``
+            token is already available in loaded word embedding.
+        unk_creation_n_least (str): number of least common word embeddings to
+            use when ``unk_creation_method`` is ``'least_common'``.
+        random_init_std (float or 'auto'): standard deviation of newly created
+            ``unk`` and ``eos`` tokens (only when they are to be randomly
+            initialized). Specify ``'auto'`` to use standard deviation of
+            the loaded word embeddings.
+        random_init_seed (int or None): Random seed that is used to create
+            unk or eos word embedding. Default is ``0`` so you get the same
+            vector every time you use the function (this is useful if you want
+            to use trained model). You should specify `None` to do experiments
+            with different seed, but you should explicitly save the word
+            embedding to use it with the trained model.
+
+    Returns:
+        vectors (numpy.ndarray): Word embedding vectors in shape of
+            ``(vocabulary size, feature dimension)``.
+        vocab (dict): Mapping from words (bytes) to vector indices (int)
+    """
+    if max_vocab is not None:
+        if eos_index >= max_vocab:
+            raise ValueError('eos_index must be within max_vocab')
+        if unk_index >= max_vocab:
+            raise ValueError('unk_index must be within max_vocab')
+    if unk is not None and eos is not None and eos_index == unk_index:
+        raise ValueError('eos_index and unk_index must be different')
+    EOS_CAND = b'</s>'
+    if unk is None and eos is None:
+        _vocab = vocab
+        _max_vocab = max_vocab
+    elif unk is not None and unk_creation_method == 'least_common':
+        # Just load everything
+        _vocab = None
+        _max_vocab = None
+    else:
+        if isinstance(vocab, six.string_types):
+            # it is difficult to add words to vocab so apply frequency afterward
+            _vocab = None
+            _max_vocab = None
+        elif isinstance(vocab, (list, set)):
+            _vocab = set(vocab)
+            _max_vocab = max_vocab
+            if unk is not None:
+                for w in util.UNK_ID_CANDIDATES:
+                    _vocab.add(w)
+            if eos is not None:
+                _vocab.add(EOS_CAND)
+        else:
+            _max_vocab = None
+            _vocab = None
+    we = WordEmbedding.load(path, vocab=_vocab, dtype=dtype,
+                            max_vocab=_max_vocab, format=format, binary=binary)
+    if unk is None and eos is None:
+        return we.vectors, we.vocab
+
+    if eos_index >= len(we):
+        raise ValueError('eos_index must be smaller than the word embedding size')
+    if unk_index >= len(we):
+        raise ValueError('unk_index must be smaller than the word embedding size')
+
+    if isinstance(vocab, six.string_types):
+        # vocab path was ignored
+        with open(vocab, mode='rb') as f:
+            freqs_loaded = loader.vocab.load_vocab(f)
+        # balance vocab and freqs
+        freqs = {k: freqs_loaded.get(k, 0) for k in six.iterkeys(we.vocab)}
+        max_freq = max(freqs.values())
+        we.apply_freqs(freqs)
+
+    if we.freqs is not None:
+        max_freq = max(we.freqs.values())
+
+    if unk is not None:
+        if eos is not None and unk_index > eos_index:
+            # Inserting eos will move index of unk so compensate for that
+            unk_index -= 1
+        _unk = util.infer_unk_id(we.vocab)
+        if _unk is None:
+            # Create unk
+            if unk_creation_method == 'least_common':
+                v = util.create_unk_least_common(we, unk_creation_n_least)
+            elif unk_creation_method == 'random':
+                if random_init_std == 'auto':
+                    random_init_std = np.std(we.vectors)
+                v = util.create_random_vector(
+                    we.vectors.shape[1], random_init_std,
+                    dtype=we.vectors.dtype, seed=random_init_seed)
+            else:
+                raise ValueError(
+                    'Unknown unk_creation_method %s' % unk_creation_method)
+            we.vocab, we.vectors = util.insert_vocab(
+                we.vocab, we.vectors, unk, unk_index, v)
+        else:
+            we.vocab, we.vectors, _ = util.move_vocab(
+                we.vocab, we.vectors, _unk, unk_index)
+            del we.vocab[_unk]
+            we.vocab[unk] = unk_index
+        if we.freqs is not None:
+            we.freqs[unk] = max_freq + 1
+    if eos is not None:
+        if EOS_CAND not in we.vocab:
+            # Create eos
+            if random_init_std == 'auto':
+                random_init_std = np.std(we.vectors)
+            random_init_seed_eos = (None if random_init_seed is None else
+                                    random_init_seed + 1)
+            v = util.create_random_vector(
+                we.vectors.shape[1], random_init_std, dtype=we.vectors.dtype,
+                seed=random_init_seed_eos
+            )
+            we.vocab, we.vectors = util.insert_vocab(
+                we.vocab, we.vectors, eos, eos_index, v)
+        else:
+            we.vocab, we.vectors, _ = util.move_vocab(
+                we.vocab, we.vectors, EOS_CAND, eos_index)
+            del we.vocab[EOS_CAND]
+            we.vocab[eos] = eos_index
+        if we.freqs is not None:
+            we.freqs[eos] = max_freq + 1
+
+    if isinstance(vocab, six.string_types):
+        # Prune freq = 0 by changing
+        _max_vocab = len([1 for k in six.iterkeys(we.vocab) if k in freqs_loaded])
+        if eos is not None and eos not in freqs_loaded:
+            _max_vocab += 1
+        if unk is not None and unk not in freqs_loaded:
+            _max_vocab += 1
+        max_vocab = _max_vocab if max_vocab is None else min(max_vocab, _max_vocab)
+
+    if max_vocab is not None:
+        # index of eos/unk is always smaller than max_vocab so it is safe to
+        # call resize
+        we.resize(max_vocab)
+    return we.vectors, we.vocab
